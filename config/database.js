4df83fd2bc90c258f7bd232026bfe31becb075d2
Khaled Al Shiekh <khaled.al_shiekh@ldc.lu.se>
2017-09-14 11:35:30 +0200

    we are on cloud

--- config/database.js
+++ config/database.js
@@ -1,5 +1,8 @@
 module.exports = {
   //database: 'mongodb://brad:brad@ds121190.mlab.com:21190/meanauthapp',   //prod
-  database: 'mongodb://localhost:27017/meanserver',    //dev
+ //local
+ //database: 'mongodb://localhost:27017/meanserver',    //dev
+ // cloud
+ database: 'mongodb://root:asd_123456@cluster0-shard-00-00-unhhn.mongodb.net:27017,cluster0-shard-00-01-unhhn.mongodb.net:27017,cluster0-shard-00-02-unhhn.mongodb.net:27017/meanserver?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',    //dev
   secret: 'yoursecret'
 }
